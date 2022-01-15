9ed42034ef68735fb33c352db67e1fd974c47515
Julio César Gama <juliocesargama@protonmail.com>
2022-01-14 23:35:22 -0300

    Domain model, database seed

---
+++ backend/src/main/java/br/com/dsmovie/entities/Score.java
@@ -0,0 +1,43 @@
+package br.com.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+    //ID Composto
+    @EmbeddedId
+    private ScorePK id = new ScorePK();
+    private Double value;
+    
+    public Score() {
+    }
+
+    public void setMovie(Movie movie) {
+        this.id.setMovie(movie);
+    }
+
+    public void setuser(User user) {
+        this.id.setUser(user);
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public ScorePK getId() {
+        return id;
+    }
+
+    public void setId(ScorePK id) {
+        this.id = id;
+    }
+
+}
